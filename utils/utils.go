be19101e918e53fca213bf1fbe9b743c1d1e323c
Lauro Santana <devlauross@gmail.com>
2025-10-15 21:40:14 -0300

    refactor: update column name pattern

--- utils/utils.go
+++ utils/utils.go
@@ -40,14 +40,15 @@ func namePattern(name string) string {
 	}
 
 	result := strings.Builder{}
-	result.WriteRune(unicode.ToLower(rune(name[0])))
-	for _, r := range name[1:] {
-		if unicode.IsUpper(r) {
+	result.WriteRune(rune(name[0]))
+	for i := 1; i < len(name); i++ {
+		currentLetter, prevLetter := rune(name[i]), rune(name[i-1])
+		if unicode.IsUpper(currentLetter) && unicode.IsLower(prevLetter) {
 			result.WriteRune('_')
-			result.WriteRune(unicode.ToLower(r))
-			continue
+		} else if c := i + 1; c < len(name) && (unicode.IsUpper(currentLetter) && unicode.IsLower(rune(name[c]))) {
+			result.WriteRune('_')
 		}
-		result.WriteRune(r)
+		result.WriteRune(currentLetter)
 	}
-	return result.String()
+	return strings.ToLower(result.String())
 }
